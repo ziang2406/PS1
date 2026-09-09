f538687db85953404804733411baa2e3d0e3b0ac
Maeve Nguyen <nguyen.3478@osu.edu>
2026-09-09 17:29:10 -0400

    Update problem set solution

--- code/Q1.R
+++ code/Q1.R
@@ -1,3 +1,18 @@
 rm(list=ls())
 library(tidyverse)
 
+#data loading 
+EQ <- read.csv("EQ Dataset.csv")
+dp <- EQ$dp
+dg <- EQ$dg
+re <- EQ$re
+H_max <- 20 #setting max horizons in years
+step_yr <- 12 
+n <- nrow(EQ)
+var_dp <- var(dp, na.rm = TRUE)
+
+#using our derivation in Q1 part a, we define kappa as follows:
+kappa <- 1/(1+ exp(mean(dp)))
+
+#creating the main function for the loop 
+for 
